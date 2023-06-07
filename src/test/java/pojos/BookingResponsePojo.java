dd0f432b760a73da69d316f47a7301ce91927d02
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-06-07 19:58:04 +0200

    ...

--- src/test/java/pojos/BookingResponsePojo.java
+++ src/test/java/pojos/BookingResponsePojo.java
@@ -1,5 +1,31 @@
 package pojos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BookingResponsePojo {
 
+    private BookingPojo booking;
+
+    public BookingResponsePojo(BookingPojo booking) {
+        this.booking = booking;
+    }
+
+    public BookingResponsePojo() {
+    }
+
+    public BookingPojo getBooking() {
+        return booking;
+    }
+
+    public void setBooking(BookingPojo booking) {
+        this.booking = booking;
+    }
+
+    @Override
+    public String toString() {
+        return "BookingResponsePojo{" +
+                "booking=" + booking +
+                '}';
+    }
 }
