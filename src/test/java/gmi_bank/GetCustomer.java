5a65731109bdc576ef1ca6e88e8c6b83ace1e8db
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-07-03 22:51:31 +0200

    ...

---
+++ src/test/java/gmi_bank/GetCustomer.java
@@ -0,0 +1,72 @@
+package gmi_bank;
+
+public class GetCustomer {
+
+            /*
+    Given
+        https://www.gmibank.com/api/tp-customers/133986
+    When
+        User sends Get request
+    Then
+        Status code should be 200
+     And
+        Response body should be like:
+{
+    "id": 133986,
+    "firstName": "Danika",
+    "lastName": "Huel",
+    "middleInitial": "S",
+    "email": "danikahuel@gmail.com",
+    "mobilePhoneNumber": "155-489-7844",
+    "phoneNumber": "155-489-7844",
+    "zipCode": "32476",
+    "address": "3848 Lang Hill",
+    "city": "Free City",
+    "ssn": "725-97-6213",
+    "createDate": "2022-01-21T05:00:00Z",
+    "zelleEnrolled": false,
+    "country": {
+        "id": 187679,
+        "name": "Banana",
+        "states": null
+    },
+    "state": "Apple",
+    "user": {
+        "id": 134701,
+        "login": "raymundo.moen",
+        "firstName": "Danika",
+        "lastName": "Huel",
+        "email": "danikahuel@gmail.com",
+        "activated": true,
+        "langKey": "en",
+        "imageUrl": null,
+        "resetDate": null
+    },
+    "accounts": [
+        {
+            "id": 128481,
+            "description": "Description",
+            "balance": 0,
+            "accountType": "CHECKING",
+            "accountStatusType": "ACTIVE",
+            "createDate": "2022-01-04T21:00:00Z",
+            "closedDate": "2022-01-04T21:00:00Z",
+            "employee": null,
+            "accountlogs": null
+        },
+        {
+            "id": 131776,
+            "description": "mfy",
+            "balance": 536846,
+            "accountType": "CREDIT_CARD",
+            "accountStatusType": "ACTIVE",
+            "createDate": "2022-01-18T21:00:00Z",
+            "closedDate": "2022-01-18T21:00:00Z",
+            "employee": null,
+            "accountlogs": null
+        }
+    ]
+}
+     */
+
+}
